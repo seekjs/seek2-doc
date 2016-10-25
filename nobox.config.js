084a28a7a8abd702f6e2bcf4d517afe53ce1b6a8
利开团 <likaituan@wecash.net>
2016-10-25 16:35:36 +0800

    quick submit

--- nobox.config.js
+++ nobox.config.js
@@ -15,7 +15,6 @@ module.exports = function (args) {
         },
         pub:{
             domain: "seekjs.org",
-            ip: "101.200.178.123",
             packages: ["README.md", "css", "data", "images", "index.html", "main.js", "nobox.config.js", "node_modules", "package.json", "pages", "plugins", "utils"],
             dir: "/nobox-server/seekjs-doc",
             port: 2016
