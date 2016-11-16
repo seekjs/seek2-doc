a52cb80d848cbb9ba2a4ca164a298f7d25138dce
qumin <qumin@wecash.net>
2016-11-16 18:46:26 +0800

    ca

--- nobox.config.js
+++ nobox.config.js
@@ -23,11 +23,5 @@ module.exports = function (args) {
         port: 2016
     };
 
-    if(args.f){
-        // config.static.items.unshift({path: "/node_modules/", dir: "/data/github/seekjs-plugin"});
-        config.static.items.unshift({path: "/node_modules/seekjs-plugin-flow/", dir: "f:/seek/seekaside/seekjs-plugin-flow/"});
-        // config.static.items.unshift({path: "/node_modules/seekjs/", dir: "/data/github/seekjs"});
-    }
-
     return config;
 };
