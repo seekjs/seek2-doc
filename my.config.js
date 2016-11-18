a66ce15638e082d689973a99288a79a18d91816e
利开团 <likaituan@wecash.net>
2016-11-18 17:21:16 +0800

    quick submit

--- my.config.js
+++ my.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     staticMaps: [
+        {path: "/node_modules/seekjs-plugin-dialog/", dir: "/data/github/seekjs-plugin/seekjs-plugin-dialog"},
         {path: "/node_modules/seekjs-plugin-flow/", dir: "/data/github/seekjs-plugin/seekjs-plugin-flow"},
         {path: "/node_modules/seekjs/", dir: "/data/github/seekjs"}
     ]
