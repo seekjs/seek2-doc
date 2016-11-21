98733d32754d65ec294ff7ce34ff14a37e7a408a
qumin <qumin@wecash.net>
2016-11-21 15:03:01 +0800

    ca

--- my.config.js
+++ my.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     staticMaps: [
         {path: "/node_modules/seekjs-plugin-dialog/", dir: "f:/seek/seekaside/seekjs-plugin-dialog"},
-        {path: "/node_modules/seekjs-plugin-dialog/", dir: "f:/seek/seekaside/seekjs-plugin-datePicker"},
+        {path: "/node_modules/seekjs-plugin-datePicker/", dir: "f:/seek/seekaside/seekjs-plugin-datePicker"},
         // {path: "/node_modules/seekjs-plugin-dialog/", dir: "/data/github/seekjs-plugin/seekjs-plugin-dialog"},
         // {path: "/node_modules/seekjs-plugin-flow/", dir: "/data/github/seekjs-plugin/seekjs-plugin-flow"},
         // {path: "/node_modules/seekjs/", dir: "/data/github/seekjs"}
