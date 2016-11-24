f4f47fb2b5a8b389b99b48356e15ad8ac163c7a4
利开团 <likaituan@wecash.net>
2016-11-24 18:51:11 +0800

    quick submit

--- my.config.js
+++ my.config.js
@@ -1,21 +1,8 @@
-
-
 module.exports = {
     staticMaps: [
-<<<<<<< HEAD
         {path: "/node_modules/seekjs-plugin-datepicker/", dir: "/data/github/seekjs-plugin/seekjs-plugin-datepicker"},
         {path: "/node_modules/seekjs-plugin-dialog/", dir: "/data/github/seekjs-plugin/seekjs-plugin-dialog"},
         {path: "/node_modules/seekjs-plugin-flow/", dir: "/data/github/seekjs-plugin/seekjs-plugin-flow"},
         {path: "/node_modules/seekjs/", dir: "/data/github/seekjs"}
-=======
-        {path: "/node_modules/seekjs-plugin-dialog/", dir: "f:/seek/seekaside/seekjs-plugin-dialog"},
-        {path: "/node_modules/seekjs-plugin-datePicker/", dir: "f:/seek/seekaside/seekjs-plugin-datePicker"},
-
-        // {path: "/node_modules/seekjs-plugin-datePicker/", dir: "/data/github/seekjs-plugin/seekjs-plugin-datePicker"},
-        // {path: "/node_modules/seekjs-plugin-dialog/", dir: "/data/github/seekjs-plugin/seekjs-plugin-dialog"},
-        // {path: "/node_modules/seekjs-plugin-flow/", dir: "/data/github/seekjs-plugin/seekjs-plugin-flow"},
-        // {path: "/node_modules/seekjs/", dir: "/data/github/seekjs"}
-
->>>>>>> 52d7a3d15581a86796a0599568da3d5d7d932557
     ]
 };
