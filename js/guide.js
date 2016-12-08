850a8e5aeeb34a7c19d3ad90f1cc2fe52c89f49b
利开团 <likaituan@wecash.net>
2016-12-08 18:29:40 +0800

    quick submit

--- js/guide.js
+++ js/guide.js
@@ -1,4 +1,4 @@
- exports.$lang = {
+exports.$lang = {
     guide: {
         cn: "教程",
         en: "Guide"
@@ -43,7 +43,7 @@
         cn: "事件绑定",
         en: "Event Bind"
     },
-     app: {
+    app: {
         cn: "seekApp框架",
         en: "App Framework"
     },
@@ -64,24 +64,34 @@
         en: "Environment"
     }
 };
- exports.onInit = function() {
-    exports.preList = [
-        { page: "guide-summary", name: "Summary", title: $lang.summary },
-        { page: "guide-scaffold", name: "Scaffold", title: $lang.scaffold },
-        { page: "quick-start", name: "Quick Start", title: $lang.quick_start }
+
+var env = require("sys.env");
+
+exports.onInit = function () {
+    log(this.params);
+    this.file = this.params.id || "guide-summary";
+
+    this.preList = [
+        {page: "guide-summary", name: "Summary", title: $lang.summary},
+        {page: "guide-scaffold", name: "Scaffold", title: $lang.scaffold},
+        {page: "quick-start", name: "Quick Start", title: $lang.quick_start}
     ];
-     exports.List = [
-        { page: "guide-modularize", name: "Modularize", title: $lang.modularize },
-        { page: "guide-app", name: "App", title: $lang.app },
-        { page: "guide-view", name: "View", title: $lang.view },
-        { page: "guide-template", name: "Template", title: $lang.template },
-        { page: "guide-bind", name: "Bind", title: $lang.data_bind },
-        { page: "guide-event", name: "Event", title: $lang.event_bind },
-        { page: "guide-pipe", name: "Pipe", title: $lang.pipe },
-        { page: "guide-ajax", name: "AJAX", title: $lang.ajax },
-        { page: "guide-form", name: "Form", title: $lang.form },
-        { page: "guide-env", name: "Environment", title: $lang.env },
-        { page: "guide-test", name: "Unit Test", title: $lang.unit_test },
-        { page: "guide-plugin", name: "Plugin", title: $lang.plugin }
+    this.List = [
+        {page: "guide-modularize", name: "Modularize", title: $lang.modularize},
+        {page: "guide-app", name: "App", title: $lang.app},
+        {page: "guide-view", name: "View", title: $lang.view},
+        {page: "guide-template", name: "Template", title: $lang.template},
+        {page: "guide-bind", name: "Bind", title: $lang.data_bind},
+        {page: "guide-event", name: "Event", title: $lang.event_bind},
+        {page: "guide-pipe", name: "Pipe", title: $lang.pipe},
+        {page: "guide-ajax", name: "AJAX", title: $lang.ajax},
+        {page: "guide-form", name: "Form", title: $lang.form},
+        {page: "guide-env", name: "Environment", title: $lang.env},
+        {page: "guide-test", name: "Unit Test", title: $lang.unit_test},
+        {page: "guide-plugin", name: "Plugin", title: $lang.plugin}
     ];
+};
+
+exports.go2 = function(file){
+    this.go(env.isPc ? `guide/${file}` : file);
 };
